f42c2771fe4bf6f56af1dc95cbcd98c220149100
Anderson <andersonlundberg@gmail.com>
2019-12-23 11:17:10 -0300

    eventos

--- dom/eventos/script.js
+++ dom/eventos/script.js
@@ -1,13 +1,62 @@
-//Mostre no console cada parágrafo do site
-// const paragrafos = document.querySelectorAll("p");
-// console.log(paragrafos);
+// const img = document.querySelector("img");
 
-// paragrafos.forEach(item => {
-// 	console.log(item);
+// com função anonima
+
+// img.addEventListener("click", () => {
+// 	console.log("clicou");
 // });
 
-//Mostre o texto dos paragrafos no console
+// Função separada
 
-// paragrafos.forEach(item => {
-// 	console.log(item.innerText);
+// function callback() {
+// 	console.log("clicou");
+// }
+
+// img.addEventListener("click", callback);
+
+// Event
+// function callback(event) {
+// 	console.log(event);
+// }
+
+// img.addEventListener("click", callback);
+
+// Exercicio 1
+// nao funcionou...
+// const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+// function handleLink(event) {
+// 	event.preventDefault();
+// 	this.classList.toogle("ativo");
+// }
+
+// linksInternos.forEach(link => {
+// 	link.addEventListener("click", handleLink);
 // });
+
+//exercicio 2
+//retornando todos os elementos do site
+// const tdsElementos = document.querySelectorAll("*");
+// console.log(tdsElementos);
+
+//que estão dentro de body e remover
+const tdsElementos = document.querySelectorAll("body *");
+
+function handleElemento(event) {
+	console.log(event.currentTarget); //para mostrar
+	//console.log(event.currentTarget.remove()); //para remover
+}
+
+tdsElementos.forEach(element => {
+	element.addEventListener("click", handleElemento);
+});
+
+//exercicio 3
+
+function handleClickT(event) {
+	if (event.key === "t") {
+		document.documentElement.classList.toggle("textoMaior"); //adiciona a classe que foi adicionada ao documento css e aumenta as letras da párgina
+	}
+}
+
+window.addEventListener("keydown", handleClickT);
